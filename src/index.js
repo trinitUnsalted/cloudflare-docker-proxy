1ccfa96139bb9a809214f16b8c8b0229c46092aa
YOUIMARK <91778001+trinitUnsalted@users.noreply.github.com>
2025-09-08 21:05:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.closeai.me": dockerHub,
-  "quay.closeai.me": "https://quay.io",
-  "gcr.closeai.me": "https://gcr.io",
-  "k8s-gcr.closeai.me": "https://k8s.gcr.io",
-  "k8s.closeai.me": "https://registry.k8s.io",
-  "ghcr.closeai.me": "https://ghcr.io",
-  "cloudsmith.closeai.me": "https://docker.cloudsmith.io",
+  "docker.727672.xyz": dockerHub,
+  "quay.727672.xyz": "https://quay.io",
+  "gcr.727672.xyz": "https://gcr.io",
+  "k8s-gcr.727672.xyz": "https://k8s.gcr.io",
+  "k8s.727672.xyz": "https://registry.k8s.io",
+  "ghcr.727672.xyz": "https://ghcr.io",
+  "cloudsmith.727672.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.closeai.me": dockerHub,
+  "docker-staging.727672.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
